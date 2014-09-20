00f02dafd9c7e68a47d65060c18cd7a79b945a63
Pan Feng <panfeng@live.com>
2014-09-21 00:36:38 +0900

    update for <hi> tag debug
    , and convert two xml file to array

--- xml_import.php
+++ xml_import.php
@@ -1,6 +1,7 @@
 <?php
-include './XML2Array.php';
+// include './XML2Array.php';
 include './xmlstr_to_array.php';
+// include './xml2arr_dom.php';
 
 function pre_print_r($var){
 		echo "<pre>";
@@ -9,18 +10,67 @@ function pre_print_r($var){
 	}
 
 
-
 // $part2 = new DOMDocument();
 // $part2->load("./part2.xml");
-
 // $globReg = new DOMDocument();
 // $globReg->load("./globReg.xml");
-
 //$part2_arr = XML2Array::createArray($part2);
 //$globReg_arr = XML2Array::createArray($globReg);
 
 
-$part2 = file_get_contents("./globReg.xml");
-$result = xmlstr_to_array($part2);
-pre_print_r($result);
+$globRegxml = file_get_contents("./globReg.xml");
+$part2xml = file_get_contents("./part2.xml");
+$globRegxml = xmlstr_to_array($globRegxml);
+$part2xml = xmlstr_to_array($part2xml);
 
+$file = './db_globReg.php';
+$text = '<?php $db_globReg ='.var_export($globRegxml, true).';';
+if(false !== fopen($file, 'w+')){
+    file_put_contents($file, $text);
+}else{
+    echo "FAIL!";
+}
+
+
+
+$file = './db_part2.php';
+$text = '<?php $db_globReg ='.var_export($part2xml, true).';';
+if(false !== fopen($file, 'w+')){
+    file_put_contents($file, $text);
+}else{
+    echo "FAIL!";
+}
+
+/*
+
+$str = <<<XML
+<?xml version="1.0"?>
+<group oname="Reproduktion, einfache" uname="Schema der e.R.">
+    <entry>
+        <name>Schema der e.R.</name>
+        <book>11</book>
+        <startPage>485</startPage>
+        <endPage>485</endPage>
+        <startLine>16</startLine>
+        <endLine>24</endLine>
+        <startTerm>Wir</startTerm>
+        <endTerm>
+            M
+            <hi rend="sup">29<hi rend="frac">16/21</hi>
+            </hi>β.
+        </endTerm>
+    </entry>
+</group>
+
+XML;
+
+$doc = new DOMDocument();
+$doc->loadXML($str);
+$endTerm = $doc->getElementsByTagName('endTerm');
+echo $doc->saveXML($endTerm->item(0));
+
+// $in_endterm = new DOMDocument();
+// $in_endterm->appendChild($endTerm);
+// echo $in_endterm->saveXML();
+
+*/
