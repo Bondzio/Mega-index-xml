1d9c48639a225d772aafc094e0b535009b9c08e0
Pan Feng <panfeng@live.com>
2014-09-21 02:33:17 +0900

    update array_to_file function by add getVarName

--- utility.php
+++ utility.php
@@ -6,8 +6,26 @@ function pre_print_r($var){
 		echo "</pre>";
 }   
 
-function array_to_file($fileName, $array, $arrayName){
-	// $file = './db_part2.php';
+//return variable name
+// function getVarName($var) {      
+//   $tmp = array($var => '');
+//   $keys = array_keys($tmp);
+//   return trim($keys[0]);
+// }
+
+function getVarName($var) {
+    foreach($GLOBALS as $var_name => $value) {
+        if ($value === $var) {
+            return $var_name; } }
+    return false;
+}
+
+
+
+//@arrayName: string of name of $array;
+function array_to_file($array){
+	$arrayName = getVarName($array);
+	$fileName = 'db_'.$arrayName.".php";
 	$text = '<?php $'.$arrayName.' ='.var_export($array, true).';';
 	if(false !== fopen($fileName, 'w+')){
 	    file_put_contents($fileName, $text);
