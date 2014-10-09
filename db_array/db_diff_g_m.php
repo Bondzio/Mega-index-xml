932267a1d5a4f7de8ec446cae712cb056a3ee1de
Pan Feng <panfeng@live.com>
2014-10-09 17:19:36 +0900

    compare index with oname from globReg.xml
    index from file of "text/tohoku_mega_index.txt",
    the result is different. it's seems that word started with German
    special alphabet like Ä or lower alphabet are not display on the mega
    website.

---
+++ db_array/db_diff_g_m.php
@@ -0,0 +1,13 @@
+<?php $diff_g_m =array (
+  8 => 'Ägypten',
+  27 => 'Äquivalentform, allgemeine (Äquivalent, allgemeines)',
+  72 => 'avances',
+  127 => 'differentia specifica',
+  158 => 'faux frais',
+  441 => 'Österreich',
+  616 => 'Übereinfuhr',
+  617 => 'Überproduktion',
+  618 => 'Übervölkerung, relative',
+  619 => 'Überzufuhr',
+  639 => 'verschleiss des Geldes',
+);
--- globRegVolcabulary.php
+++ globRegVolcabulary.php
@@ -10,10 +10,34 @@ for ($i=0; $i < count($globRegxml['oberBegriff']); $i++) {
 }
 
 
-pre_print_r($globReg_volcabulary);
+// pre_print_r($globReg_volcabulary);
 array_to_file($globReg_volcabulary);
 // array_unique($globReg_volcabulary);
 // pre_print_r($globReg_volcabulary);
 
+$mega_tohoku = file("./text/tohoku_mega_index.txt");
+$mega = array();
+foreach ($mega_tohoku as $word) {
+	// trim "0d0a" use trimUTF8BOM
+	$mega[] = trimUTF8BOM($word);
+}
 
+// delete special word "„Das Kapital”" to compare two array
+$special_word = "„Das Kapital”";
+$pos = array_search($special_word, $mega);
+unset($mega[$pos]);
+$pos = array_search($special_word, $globReg_volcabulary);
+unset($globReg_volcabulary[$pos]);
 
+$mega = array_values($mega);
+$globReg_volcabulary = array_values($globReg_volcabulary);
+
+// echo $mega==$globReg_volcabulary?"Same":"Different";
+
+// pre_print_r($mega);
+// pre_print_r($globReg_volcabulary);
+
+$diff_m_g = array_diff($mega, $globReg_volcabulary); //empty
+$diff_g_m = array_diff($globReg_volcabulary, $mega);
+pre_print_r($diff_g_m);
+array_to_file($diff_g_m);
