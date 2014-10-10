38d181fb8f21e3dd6fa35d724fd212d40af62ec5
Pan Feng <panfeng@live.com>
2014-10-10 21:53:37 +0900

    clean unnecessary comment

--- xml_import.php
+++ xml_import.php
@@ -1,74 +1,15 @@
 <?php
-// include './XML2Array.php';
 include './xmlstr_to_array.php';
-include './utility.php';
-// include './xml2arr_dom.php';
+require_once('./utility.php');
 
 
 $globRegxml = file_get_contents("./xml/globReg.xml");
-
-// $part2xml = file_get_contents("./xml/part2.xml");
-$tmpxml = file_get_contents("./xml/tmp.xml");
+$tmpxml = file_get_contents("./tmp/tmp.xml");
 
 
 $globRegxml = xmlstr_to_array($globRegxml);
-// $part2xml = xmlstr_to_array($part2xml);
 $tmpxml = xmlstr_to_array($tmpxml);
 
 array_to_file($globRegxml);
-// array_to_file($part2xml);
 array_to_file($tmpxml);
-
-
-
-// $file = './db_globReg.php';
-// $text = '<?php $db_globReg ='.var_export($globRegxml, true).';';
-// if(false !== fopen($file, 'w+')){
-//     file_put_contents($file, $text);
-// }else{
-//     echo "FAIL!";
-// }
-
-
-
-// $file = './db_part2.php';
-// $text = '<?php $db_globReg ='.var_export($part2xml, true).';';
-// if(false !== fopen($file, 'w+')){
-//     file_put_contents($file, $text);
-// }else{
-//     echo "FAIL!";
-// }
-
-/*
-
-$str = <<<XML
-<?xml version="1.0"?>
-<group oname="Reproduktion, einfache" uname="Schema der e.R.">
-    <entry>
-        <name>Schema der e.R.</name>
-        <book>11</book>
-        <startPage>485</startPage>
-        <endPage>485</endPage>
-        <startLine>16</startLine>
-        <endLine>24</endLine>
-        <startTerm>Wir</startTerm>
-        <endTerm>
-            M
-            <hi rend="sup">29<hi rend="frac">16/21</hi>
-            </hi>β.
-        </endTerm>
-    </entry>
-</group>
-
-XML;
-
-$doc = new DOMDocument();
-$doc->loadXML($str);
-$endTerm = $doc->getElementsByTagName('endTerm');
-echo $doc->saveXML($endTerm->item(0));
-
-// $in_endterm = new DOMDocument();
-// $in_endterm->appendChild($endTerm);
-// echo $in_endterm->saveXML();
-
-*/
+header("Location: ./array_merge.php");
