17ae6b2a9dbde63761f5524e0eee855d096349d0
Pan Feng <panfeng@live.com>
2014-10-10 21:51:40 +0900

    Code refactoring: all download url can use this
    just add the referer to switch case ...

--- download.php
+++ download.php
@@ -1,5 +1,22 @@
 <?php
-    $filePath = './tmp/tmp.xml';
+// var_dump($_SERVER['HTTP_REFERER']);
+$url = $_SERVER['HTTP_REFERER'];
+$arr = explode("/", $url);
+$key = $arr[count($arr)-1];
+
+switch ($key) {
+    case 'xml_export.php':
+        $filePath = './tmp/tmp.xml';
+        break;
+    case 'xml_import.php':
+    // to be update
+        $filePath = './tmp/tmp.xml';
+        break;
+    default:
+        # code...
+        break;
+}
+
 
     if(file_exists($filePath)) {
         $fileName = basename($filePath);
