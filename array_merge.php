fcfa49001bd7b4f22d612ac9cf291ed8b03352d9
Pan Feng <panfeng@live.com>
2014-10-13 21:01:04 +0900

    recode array_merge.php for
    array_merge_recursive  seems wrong, still thinking about it

--- array_merge.php
+++ array_merge.php
@@ -3,8 +3,43 @@ include './db_array/db_globRegxml.php';
 include './db_array/db_tmpxml.php';
 require_once('./utility.php');
 
+$globReg_o = array();
+$tmp_o = array();
 
-$result =  array_merge_recursive($globRegxml, $tmpxml);
+for ($i=0; $i < count($globRegxml['oberBegriff']); $i++) { 
+	$globReg_o[] = trimUTF8BOM($globRegxml['oberBegriff'][$i]['oname']);
+}
+
+for ($i=0; $i < count($tmpxml['oberBegriff']); $i++) { 
+	$tmp_o[] = trimUTF8BOM($tmpxml['oberBegriff'][$i]['oname']);
+}
+
+// array_to_file($globReg_o);
+// array_to_file($tmp_o);
+
+$merge_arr = array_merge($globReg_o,$tmp_o);
+$volcabulary_id = array_unique($merge_arr);
+array_unshift($volcabulary_id, "");
+unset($volcabulary_id[0]);
+
+$duplicate_word = array_intersect($globReg_o, $tmp_o);
+$duplicate_word = array_values($duplicate_word);
+array_unshift($duplicate_word, "");
+unset($duplicate_word[0]);
+
+array_to_file($duplicate_word);
+array_to_file($volcabulary_id);
+
+pre_print_r("<h3>ソートした合成の単語リスト（重複なし）</h3>");
+pre_print_r($volcabulary_id);
+// pre_print_r($duplicate_word);
+
+// count($merge_arr)-count(array_unique($globReg_o,$tmp_o)) == count($dupl_arr);
+
+// something is wrong here
+// $result =  array_merge_recursive($globRegxml, $tmpxml);
+// array_to_file($result);
+
 // pre_print_r("<h3><a href=''>合成したXMLファイルをダウンロードします</a></h3>");
 // pre_print_r($result['oberBegriff']);
 
@@ -26,26 +61,25 @@ $result =  array_merge_recursive($globRegxml, $tmpxml);
 // pre_print_r(bin2hex("”"));
 // pre_print_r(hex2bin(bin2hex("„")));
 
-$volcabulary = array();
+// $volcabulary = array();
 
-for ($i=0; $i < count($result['oberBegriff']); $i++) { 
-	$volcabulary[] = trimUTF8BOM($result['oberBegriff'][$i]['oname']);
-}
+// for ($i=0; $i < count($result['oberBegriff']); $i++) { 
+// 	$volcabulary[] = trimUTF8BOM($result['oberBegriff'][$i]['oname']);
+// }
 
-// pre_print_r($volcabulary);
 
 // comepare this two output, some word are 
 // pre_print_r(array_unique($volcabulary));
 // https://www.diffchecker.com/e39cyuaj
 
-$duplicate_word = array_diff($volcabulary, array_unique($volcabulary));
+// $duplicate_word = array_diff($volcabulary, array_unique($volcabulary));
 // array_intersect_assoc() // array_intersect() // array_diff_assoc // array_intersect
 // pre_print_r($duplicate_word);
-array_to_file($duplicate_word);
+// array_to_file($duplicate_word);
 // function here to merge those word;
 // to be done...
 
-$unique_volcabulary = array_unique($volcabulary);
+// $unique_volcabulary = array_unique($volcabulary);
 // echo $string1 = $unique_volcabulary[734]; //UTF-8
 // echo $string2 = $unique_volcabulary[235]; //ASCII
 // $string1=utf8_decode($string1);
@@ -54,20 +88,19 @@ $unique_volcabulary = array_unique($volcabulary);
 // var_dump($string2);
 
 // http://zh.wikipedia.org/wiki/%E5%BE%B7%E8%AA%9E%E5%AD%97%E6%AF%8D
-$volcabulary_id = array(0=>'');
-	foreach ($unique_volcabulary as $item) {
-		$volcabulary_id[] = $item;
-		$cha=mb_detect_encoding($item);
-		if($cha == 'ASCII'){
-			$item = utf8_encode($item);
-		}
-		// echo utf8_encode($cha)." : ".$item."<br />";
-	}
-unset($volcabulary_id[0]);
-pre_print_r("<h3>ソートした合成の単語リスト（重複なし）</h3>");
-pre_print_r($volcabulary_id);
+// $volcabulary_id = array(0=>'');
+// 	foreach ($unique_volcabulary as $item) {
+// 		$volcabulary_id[] = $item;
+// 		$cha=mb_detect_encoding($item);
+// 		if($cha == 'ASCII'){
+// 			$item = utf8_encode($item);
+// 		}
+// 		// echo utf8_encode($cha)." : ".$item."<br />";
+// 	}
+// unset($volcabulary_id[0]);
+// pre_print_r($volcabulary_id);
 
-array_to_file($volcabulary_id);
+// array_to_file($volcabulary_id);
 // asort($volcabulary_id);
 // pre_print_r($volcabulary_id);
 // natcasesort($volcabulary_id);
