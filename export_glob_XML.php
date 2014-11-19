a61f3203eb21fa5232b5bb4614539e42ea655e6d
Pan Feng <scr.im/meditator>
2014-11-20 00:38:05 +0900

    export to final xml, to be done;

---
+++ export_glob_XML.php
@@ -0,0 +1,208 @@
+<?php
+include './db_array/db_tmp_glob_arr.php';
+require_once('./utility.php');
+
+$dom = new DomDocument('1.0', 'UTF-8');
+$newGlobRegXML = $dom->appendChild($dom->createElement('globReg'));
+
+
+// pre_print_r($tmp_glob_arr['oberBegriff'][3]['unterBegriff']);
+// pre_print_r($tmp_glob_arr['oberBegriff'][3]['unterBegriff'][0]['uname']);
+// pre_print_r($tmp_glob_arr['oberBegriff'][3]['unterBegriff'][1]['uname']);
+// pre_print_r($tmp_glob_arr['oberBegriff'][4]['unterBegriff']['uname']);
+// pre_print_r($tmp_glob_arr['oberBegriff'][4]);
+// pre_print_r($tmp_glob_arr['oberBegriff'][3]);
+
+// pre_print_r($tmp_glob_arr['oberBegriff'][0]['unterBegriff'][6]['uname']);
+
+
+foreach ($tmp_glob_arr as $ober_key => $ober_value) {
+// foreach ($tmp_glob_arr as $oberBegriff) {
+	pre_print_r($ober_key);
+
+
+// 	$ober = $newGlobRegXML->appendChild($dom->createElement('oberBegriff'));
+// 	$oname = $ober->appendChild($dom->createElement('oname', $oberBegriff['oname']));
+// 	// link to append to the last line of this funciton;
+// 	// $unter = $ober->appendChild($dom->createElement('unterBegriff'));
+
+
+// if(isset($oberBegriff['unterBegriff'])){
+
+
+// 	// $unter = $ober->appendChild($dom->createElement('unterBegriff'));
+	
+// 	if(isset($oberBegriff['unterBegriff']['uname'])){
+// 			// pre_print_r($oberBegriff);
+// 			$unter = $ober->appendChild($dom->createElement('unterBegriff'));
+// 			$uname = $unter->appendChild($dom->createElement('uname', $oberBegriff['unterBegriff']['uname']));
+// 			// $group = $unter->appendChild($dom->createElement('group'));
+// 			// $group->setAttribute('oname', $oberBegriff['oname']);
+// 			// $group->setAttribute('uname', $oberBegriff['unterBegriff']['uname']);
+
+// 			if(isset($oberBegriff['unterBegriff']['group']['entry'])){
+// 				// pre_print_r($oberBegriff['unterBegriff']['group']['entry']);
+// 				$group = $unter->appendChild($dom->createElement('group'));
+// 				$group->setAttribute('oname', $oberBegriff['oname']);
+// 				$group->setAttribute('uname', $oberBegriff['unterBegriff']['uname']);
+
+// 				$entry = $group->appendChild($dom->createElement('entry'));
+// 				$name = $entry->appendChild($dom->createElement('name', $oberBegriff['unterBegriff']['group']['entry']['name']));
+// 				$book = $entry->appendChild($dom->createElement('book', $oberBegriff['unterBegriff']['group']['entry']['book']));
+// 				$startPage = $entry->appendChild($dom->createElement('startPage', $oberBegriff['unterBegriff']['group']['entry']['startPage']));
+// 				$endPage = $entry->appendChild($dom->createElement('endPage'));
+// 				$startLine = $entry->appendChild($dom->createElement('startLine', ' '));
+// 				$endLine = $entry->appendChild($dom->createElement('endLine'));
+// 				$startTerm = $entry->appendChild($dom->createElement('startTerm', ' '));
+// 				$endTerm = $entry->appendChild($dom->createElement('endTerm'));
+
+// 			}else{
+// 				// pre_print_r($oberBegriff['unterBegriff']['group']);
+// 				foreach ($oberBegriff['unterBegriff']['group'] as $entry) {
+// 					// $unter = $ober->appendChild($dom->createElement('unterBegriff'));
+// 					// pre_print_r($entry['entry']);
+// 					$group = $unter->appendChild($dom->createElement('group'));
+// 					$group->setAttribute('oname', $oberBegriff['oname']);
+// 					$group->setAttribute('uname', $oberBegriff['unterBegriff']['uname']);
+
+// 					$name_value = $entry['entry']['name'];
+// 					$book_value = $entry['entry']['book'];
+// 					$startPage_value = $entry['entry']['startPage'];
+
+// 					$entry = $group->appendChild($dom->createElement('entry'));
+// 					$name = $entry->appendChild($dom->createElement('name', $name_value));
+// 					$book = $entry->appendChild($dom->createElement('book', $book_value));
+// 					$startPage = $entry->appendChild($dom->createElement('startPage', $startPage_value));
+// 					$endPage = $entry->appendChild($dom->createElement('endPage'));
+// 					$startLine = $entry->appendChild($dom->createElement('startLine', ' '));
+// 					$endLine = $entry->appendChild($dom->createElement('endLine'));
+// 					$startTerm = $entry->appendChild($dom->createElement('startTerm', ' '));
+// 					$endTerm = $entry->appendChild($dom->createElement('endTerm'));
+
+// 				}
+// 			}
+
+
+// 		}else{
+
+
+// 			// uname is array;
+// 		foreach ($oberBegriff['unterBegriff'] as $unterBegriff) {
+// 			// pre_print_r($unterBegriff['uname']);
+// 			// pre_print_r($unterBegriff);
+// 			$unter = $ober->appendChild($dom->createElement('unterBegriff'));
+// 			$uname = $unter->appendChild($dom->createElement('uname', $unterBegriff['uname']));
+
+// 			if (isset($unterBegriff['group']['entry'])) {
+// 				// pre_print_r($unterBegriff['group']['entry']['name']);
+// 				// pre_print_r($unterBegriff['group']['entry']['startPage']);
+// 				$group = $unter->appendChild($dom->createElement('group'));
+// 				$group->setAttribute('oname', $oberBegriff['oname']);
+// 				$group->setAttribute('uname', $unterBegriff['uname']);
+
+
+// 				$entry = $group->appendChild($dom->createElement('entry'));
+// 				$name = $entry->appendChild($dom->createElement('name', $unterBegriff['group']['entry']['name']));
+// 				$book = $entry->appendChild($dom->createElement('book', $unterBegriff['group']['entry']['book']));
+// 				$startPage = $entry->appendChild($dom->createElement('startPage', $unterBegriff['group']['entry']['startPage']));
+// 				$endPage = $entry->appendChild($dom->createElement('endPage'));
+// 				$startLine = $entry->appendChild($dom->createElement('startLine', ' '));
+// 				$endLine = $entry->appendChild($dom->createElement('endLine'));
+// 				$startTerm = $entry->appendChild($dom->createElement('startTerm', ' '));
+// 				$endTerm = $entry->appendChild($dom->createElement('endTerm'));
+// 				// foreach ($unterBegriff as $unterBegriff_i) {// pre_print_r($unterBegriff_i['uname']); }
+// 				// $group = $unter->appendChild($dom->createElement('group'));
+// 				// $entry = $group->appendChild($dom->createElement('entry'));
+
+// 			}else{
+
+// 					// $group = $unter->appendChild($dom->createElement('group'));
+// 					// $group->setAttribute('oname', $oberBegriff['oname']);
+// 					// $group->setAttribute('uname', $unterBegriff['uname']);
+
+// 					// pre_print_r($unterBegriff);
+
+// 				foreach ($unterBegriff['group'] as $entry_arr) {
+// 					// pre_print_r($entry_arr['entry']['startPage']);
+// 					// pre_print_r($entry_arr['entry']);
+// 					$group = $unter->appendChild($dom->createElement('group'));
+// 					$group->setAttribute('oname', $oberBegriff['oname']);
+// 					$group->setAttribute('uname', $unterBegriff['uname']);
+
+// 					$entry = $group->appendChild($dom->createElement('entry'));
+// 					$name = $entry->appendChild($dom->createElement('name', $entry_arr['entry']['name']));
+// 					$book = $entry->appendChild($dom->createElement('book', $entry_arr['entry']['book']));
+// 					$startPage = $entry->appendChild($dom->createElement('startPage', $entry_arr['entry']['startPage']));
+// 					$endPage = $entry->appendChild($dom->createElement('endPage'));
+// 					$startLine = $entry->appendChild($dom->createElement('startLine', ' '));
+// 					$endLine = $entry->appendChild($dom->createElement('endLine'));
+// 					$startTerm = $entry->appendChild($dom->createElement('startTerm', ' '));
+// 					$endTerm = $entry->appendChild($dom->createElement('endTerm'));
+// 					// foreach ($unterBegriff as $unterBegriff_i) {// pre_print_r($unterBegriff_i['uname']); }
+// 					// $group = $unter->appendChild($dom->createElement('group'));
+// 					// $entry = $group->appendChild($dom->createElement('entry'));
+// 				}
+// 		}
+
+
+
+
+// 	}}
+// 		if(isset($oberBegriff['link'])){
+// 			if(isset($oberBegriff['link']['@content'])){
+// 				// pre_print_r($oberBegriff['link']);
+// 				$link = $ober->appendChild($dom->createElement('link', trim($oberBegriff['link']['@content'])));
+// 				//$oberBegriff['link']['@content']) = array( target = '');
+// 				// 
+// 				$link->setAttribute('target', '');
+// 			}else{
+// 				foreach ($oberBegriff['link'] as $link_arr) {
+// 					// pre_print_r($link_arr['@content']);
+// 					$link = $ober->appendChild($dom->createElement('link', trim($link_arr['@content'])));
+// 					$link->setAttribute('target', '');
+// 				}
+// 			}
+
+// 		}else{
+// 			//exception: Hypothese 
+// 			// pre_print_r($oberBegriff['oname']." don't have link!");
+// 		}
+
+
+
+// 	// $ober = $globReg->appendChild($dom->createElement('oberBegriff'));
+// 	// $oname = $ober->appendChild($dom->createElement('oname', $key_char[$i]));
+
+// 	}else{
+// 		// those words don't have unter
+// 		//Exp:  Handelsfreiheit
+// 		//		#Freihandel
+
+// 		if(isset($oberBegriff['link'])){
+// 				if(isset($oberBegriff['link']['@content'])){
+// 					// pre_print_r($oberBegriff['link']);
+// 					$link = $ober->appendChild($dom->createElement('link', trim($oberBegriff['link']['@content'])));
+// 					//$oberBegriff['link']['@content']) = array( target = '');
+// 					// 
+// 					$link->setAttribute('target', '');
+// 				}else{
+// 					foreach ($oberBegriff['link'] as $link_arr) {
+// 						// pre_print_r($link_arr['@content']);
+// 						$link = $ober->appendChild($dom->createElement('link', trim($link_arr['@content'])));
+// 						$link->setAttribute('target', '');
+// 					}
+// 				}
+
+// 			}else{
+// 				//exception: Hypothese 
+// 				//don't has links;
+// 				pre_print_r($oberBegriff['oname']." don't have link!");
+// 			}
+// 	}
+};
+
+
+
+
+$dom->formatOutput = true;
+$dom->save('./xml/newGlobReg.xml');
