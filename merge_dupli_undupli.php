17d08135d5b4d2480eed5feaa6ff19b6864ae6fa
Pan Feng <scr.im/meditator>
2014-11-23 17:05:54 +0900

    add book id identify method

--- merge_dupli_undupli.php
+++ merge_dupli_undupli.php
@@ -16,6 +16,22 @@ if(count($tmp_glob_arr) !== count($word_id)){
 	exit;
 }
 
+// sort entry in group by book id;
+foreach ($tmp_glob_arr as $ober_key => $oberBegriff) {
+	pre_print_r($ober_key);
+	if(isset($oberBegriff['unterBegriff'])){
+				foreach ($oberBegriff['unterBegriff'] as $unter_key => $unterBegriff) {
+					pre_print_r($unterBegriff['uname']);
+					foreach ($unterBegriff['entry'] as $entry_key => $entry_value) {
+						$book_num = $entry_value;
+					// pre_print_r('ENTERY_KEY  =>  '.$entry_key);
+					pre_print_r('BOOK_NUM  =>  '.$entry_value['book']);
+					}
+					pre_print_r('_____________________');
+				}
+					pre_print_r('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+	}
+}
 
 // pre_print_r($tmp_glob_arr);
 array_to_file($tmp_glob_arr);
